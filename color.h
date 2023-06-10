0615d02f8d9d9d2832857becf2d230c68c81f5a1
Jorge Linares <jorgelinaresrrs306@hotmail.com>
2023-06-10 08:40:31 -0500

    cambio de menú

--- color.h
+++ color.h
@@ -16,15 +16,15 @@ void asignarColor(int codigoColor) {
     case 3: Console::ForegroundColor = ConsoleColor::DarkGreen; break;
     case 4: Console::ForegroundColor = ConsoleColor::DarkRed; break;
     case 5: Console::ForegroundColor = ConsoleColor::DarkMagenta; break;
-    case 6: Console::ForegroundColor = ConsoleColor::DarkYellow; break;
-    case 7: Console::ForegroundColor = ConsoleColor::Gray; break;
-    case 8: Console::ForegroundColor = ConsoleColor::DarkGray; break;
-    case 9: Console::ForegroundColor = ConsoleColor::Blue; break;
-    case 10: Console::ForegroundColor = ConsoleColor::Green; break;
-    case 11: Console::ForegroundColor = ConsoleColor::Cyan; break;
-    case 12: Console::ForegroundColor = ConsoleColor::Red; break;
-    case 13: Console::ForegroundColor = ConsoleColor::Magenta; break;
-    case 14: Console::ForegroundColor = ConsoleColor::Yellow; break;
-    case 15: Console::ForegroundColor = ConsoleColor::White; break;
+    case 6: Console::ForegroundColor = ConsoleColor::Gray; break;
+    case 7: Console::ForegroundColor = ConsoleColor::DarkGray; break;
+    case 8: Console::ForegroundColor = ConsoleColor::Blue; break;
+    case 9: Console::ForegroundColor = ConsoleColor::Green; break;
+    case 10: Console::ForegroundColor = ConsoleColor::Cyan; break;
+    case 11: Console::ForegroundColor = ConsoleColor::Red; break;
+    case 12: Console::ForegroundColor = ConsoleColor::Magenta; break;
+    case 13: Console::ForegroundColor = ConsoleColor::Yellow; break;
+    case 14: Console::ForegroundColor = ConsoleColor::White; break;
+    case 15: Console::ForegroundColor = ConsoleColor::DarkYellow; break;
     }
 }
--- menu.h
+++ menu.h
@@ -1,97 +1,152 @@
 #pragma once
 #include "pch.h"
 #include <iostream>
+#include <conio.h>
 #include <windows.h>
 #include "color.h"
 #include "jugar.h"
 
 
 using namespace std;
-
-void jugar()
-{
-    cout << "Iniciando el juego..." << endl;
-    // Aquí puedes llamar a la función para empezar el juego
-}
-
-void mostrarHistoria()
-{
-    cout << "Historia del juego..." << endl;
-    // Aquí puedes mostrar la historia del juego
-}
-void mostrarInstrucciones()
-{
-    cout << "Instrucciones del juego..." << endl;
-    // Aquí puedes mostrar las instrucciones del juego
-}
-void mostrarCreditos()
-{
-    cout << "Créditos del juego..." << endl;
-    // Aquí puedes mostrar los créditos del juego
-}
+using namespace System;
 
 void mostrarMenu()
 {
-    //system("cls"); // Limpiar la consola en Windows (para otros sistemas operativos, se necesita otro enfoque)
+    system("cls"); // Limpiar la consola en Windows (para otros sistemas operativos, se necesita otro enfoque)
 
     // Establecer el color del marco y las palabras
-    asignarColor(14); // Amarillo
-    cout << char(201) << char(205) << char(205) << char(205) << char(205)
+    asignarColor(8);
+
+    cout << "\n\n\t\t\t\t\t\t"; cout << char(201) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << endl;
-    cout << char(186) << "      P O W E R       " << char(186) << endl;
-    cout << char(186) << "      P L A N E T     " << char(186) << endl;
-    cout << char(204) << string(22, char(205)) << char(185) << endl;
-    cout << char(186) << " 1. Jugar             " << char(186) << endl;
-    cout << char(186) << " 2. Historia          " << char(186) << endl;
-    cout << char(186) << " 3. Instrucciones     " << char(186) << endl;
-    cout << char(186) << " 4. Créditos          " << char(186) << endl;
-    cout << char(186) << " 5. Salir             " << char(186) << endl;
-    cout << char(200) << char(205) << char(205) << char(205) << char(205)
+    cout << "\t\t\t\t\t\t"; cout << char(186); asignarColor(10); cout << char(178) << char(178) << char(178) << char(178) << char(178) << char(178); asignarColor(13); cout << " P O W E R ";
+    asignarColor(10); cout << char(178) << char(178) << char(178) << char(178) << char(178); asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; cout << char(186); asignarColor(10); cout << char(178) << char(178) << char(178) << char(178) << char(178); asignarColor(13); cout << "  S A V E R  ";
+    asignarColor(10); cout << char(178) << char(178) << char(178) << char(178); asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; cout << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+        << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+        << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << endl;
+    cout << "\t\t\t\t\t\t"; asignarColor(8); cout << char(186); asignarColor(13); cout << " 1.";
+    asignarColor(10); cout << " Jugar             "; asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; asignarColor(8); cout << char(186); asignarColor(13); cout << "  2.";
+    asignarColor(10); cout << " Historia         "; asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; asignarColor(8); cout << char(186); asignarColor(13); cout << "   3.";
+    asignarColor(10); cout << " Instrucciones   "; asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; asignarColor(8); cout << char(186); asignarColor(13); cout << "    4.";
+    asignarColor(10); cout << " Creditos       "; asignarColor(8); cout << char(186) << endl;
+    cout << "\t\t\t\t\t\t"; asignarColor(8); cout << char(186); asignarColor(13); cout << "     5.";
+    asignarColor(10); cout << " Salir         "; asignarColor(8); cout << char(186) << endl;
+    asignarColor(8);
+    cout << "\t\t\t\t\t\t"; cout << char(200) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
         << char(205) << char(205) << char(205) << char(205) << char(188) << endl;
 
     // Restaurar el color predeterminado
-    asignarColor(7); // Blanco
+    asignarColor(14); // Blanco
 }
+
+void jugar() {
+    cout << "Iniciando el juego..." << endl;
+    iniciarJuego();
+    // Aquí puedes llamar a la función para empezar el juego
+}
+void mostrarHistoria() {
+    cout << "Historia del juego..." << endl;
+    // Aquí puedes mostrar la historia del juego
+}
+
+void mostrarInstrucciones() {
+    cout << "\n\n";
+    cout << "\t    -------------------------------------------------------------------------------" << endl;
+    cout << "\t      -----------------------------------------------------------------------------------" << endl;
+    cout << "\t\tEn PowerPlanet, seras el heroe de la energia sostenible. Tu mision es ampliar la" << endl;
+    cout << "\t\tinfraestructura energetica en paises poco desarrollados. Enfrentaras obstaculos," << endl;
+    cout << "\t\tenemigos y contaras con poderes y aliados para lograrlo. A medida que avanzas, el " << endl;
+    cout << "\t\tjuego se vuelve más desafiante, haciendo que la experiencia sea aun mas emocionante." << endl;
+    cout << "\t\tPreparate para convertirte en el salvador de la energia sostenible en PowerPlanet." << endl;
+    cout << "\t\t  -----------------------------------------------------------------------------------" << endl;
+    cout << "\t\t\t-------------------------------------------------------------------------------" << endl;
+
+    cout << "\n\n\t\t\t\t\t\t\t\t\t\tPulsa una tecla para continuar...";
+
+
+    // Aquí puedes mostrar las instrucciones del juego
+}
+void mostrarCreditos() {
+
+    cout << "C R " << char(144) << "D I T O S" << endl;
+
+    cout << "1. Leandro Arevalo Nu" << char(164) << "ez" << endl;
+    cout << "2. Nayara Maldonado" << endl;
+    cout << "3. Jorge Linares" << endl;
+    // Aquí puedes mostrar los créditos del juego
+}
+
+
 int mainMenu()
 {
     int opcion;
+    mostrarMenu();
 
     do
     {
-
         mostrarMenu();
-        asignarColor(11); // Cian
-        cout << "Ingrese una opción: ";
-        cin >> opcion;
 
+
+        cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n";
+        asignarColor(5);
+        cout << "\t\t\t\t\t\t\t\t\t\t\t#########*#" << endl;
+        asignarColor(12);
+        cout << "\t\t\t\t\t\t\t\t\t\t\tIngrese una opcion: "; cin >> opcion;
+        system("cls");
+
+        asignarColor(14);
         switch (opcion)
         {
         case 1:
             jugar();
-            iniciarJuego();
+            _getch();
+
             // Aquí puedes llamar a la función para empezar el juego
             break;
         case 2:
             mostrarHistoria();
+            _getch();
+            system("cls");
+
             // Aquí puedes mostrar la historia del juego
             break;
         case 3:
             mostrarInstrucciones();
+            _getch();
+            system("cls");
             // Aquí puedes mostrar las instrucciones del juego
             break;
         case 4:
             mostrarCreditos();
+            _getch();
+            system("cls");
             // Aquí puedes mostrar los créditos del juego
             break;
         case 5:
+            asignarColor(11);
+            cout << "Game Over" << endl;
             cout << "Saliendo del juego..." << endl;
+            asignarColor(14);
+
             break;
         default:
-            cout << "Opción inválida. Por favor, ingrese un número válido." << endl;
+            asignarColor(11);
+            cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << endl;
+            cout << "\t\t\t\tOpcion invalida. Por favor, ingrese un numero valido." << endl;
+            cout << "\t\t\t\tXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << endl;
+            asignarColor(14);
+            cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tPulsa una tecla para continuar...";
+            _getch();
+            system("cls");
             break;
         }
 
@@ -99,6 +154,7 @@ int mainMenu()
 
 
     } while (opcion != 5);
+
     exit(0);
     return 0;
 }
